f19893b6d596530fe9bf27893679591a55ba696d
Denis Lebedev <d2.lebedev@gmail.com>
2013-03-09 20:10:07 +0300

    Fill fetch request info with data

--- CoreDataUtil/FetchRequestInfoController.h
+++ CoreDataUtil/FetchRequestInfoController.h
@@ -8,11 +8,16 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface FetchRequestInfoController : NSWindowController
+@interface FetchRequestInfoController : NSWindowController <NSTableViewDataSource, NSTableViewDelegate>
 
 @property (weak) IBOutlet NSTextField *fetchTemplateNameTextField;
+@property (weak) IBOutlet NSButton *entityButton;
+@property (weak) IBOutlet NSTableView *fetchPropertiesTableView;
+@property (weak) IBOutlet NSTextField *predicateTextField;
 
 - (void)show:(NSWindow *)sender forFetchRequest:(NSFetchRequest *)initial title:(NSString *)title;
+
 - (IBAction)closeAction:(id)sender;
+- (IBAction)entityClickAction:(NSButton *)sender;
 
 @end
--- CoreDataUtil/FetchRequestInfoController.m
+++ CoreDataUtil/FetchRequestInfoController.m
@@ -7,11 +7,14 @@
 //
 
 #import "FetchRequestInfoController.h"
+#import "GetInfoSheetController.h"
 
 @interface FetchRequestInfoController ()
 
 @property (strong) NSFetchRequest *fetchRequest;
 @property (strong) NSString *templateTitle;
+
+- (void)addTableColumnWithIdentifier:(NSString *)ident;
 
 @end
 
@@ -20,6 +23,11 @@
 - (void)initialize
 {
 	self.fetchTemplateNameTextField.stringValue = self.templateTitle;
+	self.predicateTextField.stringValue = [self.fetchRequest.predicate predicateFormat];
+	[self.entityButton setTitle:[self.fetchRequest.entity name]];
+	
+	[self addTableColumnWithIdentifier:@"Property"];
+    [self addTableColumnWithIdentifier:@"Value"];
 }
 
 - (void)windowDidLoad
@@ -46,4 +54,89 @@
     [self.window close];
 }
 
+- (IBAction)entityClickAction:(NSButton *)sender
+{
+	GetInfoSheetController* infoSheetController = [[GetInfoSheetController alloc] initWithWindowNibName:@"GetInfoSheetController"];
+    [infoSheetController show:self.window :self.fetchRequest.entity];
+}
+
+#pragma mark
+#pragma NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+    return 3;
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+    if ([[aTableColumn identifier] isEqualToString:@"Property"])
+    {
+		switch (rowIndex) {
+			case 0:
+				return @"Result Type";
+			case 1:
+				return @"Fetch Limit";
+			case 2:
+				return @"Batch Size";
+		}
+    }
+    else
+    {
+        switch (rowIndex) {
+			case 0:
+				return [self stringResultType:self.fetchRequest.resultType];
+			case 1:
+				return @(self.fetchRequest.fetchLimit);
+			case 2:
+				return @(self.fetchRequest.fetchBatchSize);
+		}
+    }
+	return nil;
+}
+
+#pragma mark
+#pragma NSTableViewDelegate
+
+- (BOOL)tableView:(NSTableView *)tableView shouldEditTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    return NO;
+}
+
+
+- (void)addTableColumnWithIdentifier:(NSString *)ident
+{
+    NSTableColumn *newColumn = [[NSTableColumn alloc] initWithIdentifier:ident];
+    
+    [[newColumn headerCell] setTitle:ident];
+	if ([ident isEqualToString:@"Property"])
+	{
+		newColumn.width = 200;
+	} else
+	{
+		[newColumn sizeToFit];
+	}
+    
+    [[self fetchPropertiesTableView] addTableColumn:newColumn];
+}
+
+#pragma mark 
+#pragma Private
+
+- (NSString *)stringResultType:(NSFetchRequestResultType)type
+{
+	if (type == NSManagedObjectResultType) {
+		return @"NSManagedObjectResultType";
+	}
+	if (type == NSManagedObjectIDResultType) {
+		return @"NSManagedObjectIDResultType";
+	}
+	if (type == NSDictionaryResultType) {
+		return @"NSDictionaryResultType";
+	}
+	if (type == NSCountResultType) {
+		return @"NSCountResultType";
+	}
+	return nil;
+}
 @end
