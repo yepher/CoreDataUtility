e6af4676a93db1ee1f52630615d22f4143b08a60
Aliaksandr Andrashuk <limejelly@yandex.ru>
2013-03-11 22:51:00 +0300

    Importing open file sheet controller

--- CoreDataUtilDBA/Document.m
+++ CoreDataUtilDBA/Document.m
@@ -7,6 +7,15 @@
 //
 
 #import "Document.h"
+#import "OpenFileSheetController.h"
+
+const float kOpenFileControllerShowDelay = 1.0;
+
+@interface Document ()<OpenFileSheetControllerDelegate>
+@property (strong) IBOutlet NSProgressIndicator *progressIndicator;
+@property (strong) IBOutlet OpenFileSheetController *openFileController;
+
+@end
 
 @implementation Document
 
@@ -30,11 +39,45 @@
 {
     [super windowControllerDidLoadNib:aController];
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    
+    [self.progressIndicator startAnimation:nil];
+    
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(kOpenFileControllerShowDelay * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        self.openFileController = [[OpenFileSheetController alloc] initWithWindowNibName:@"OpenFileSheetController"];
+        self.openFileController.delegate = self;
+        
+        [NSApp beginSheet:self.openFileController.window
+           modalForWindow:self.windowForSheet
+            modalDelegate:nil
+           didEndSelector:nil
+              contextInfo:NULL];
+    });
+    
 }
 
 + (BOOL)autosavesInPlace
 {
     return YES;
+}
+
+#pragma mark - Open file controller
+
+- (void)closeOpenFileSheetController {
+    [NSApp endSheet:self.openFileController.window];
+    [self.openFileController close];
+    self.openFileController = nil;
+}
+
+- (void)openFileSheetControllerDidCancel:(OpenFileSheetController *)controller {
+    [self closeOpenFileSheetController];
+    [self close];
+}
+- (void)openFileSheetController:(OpenFileSheetController *)controller didSelectModelURL:(NSURL *)modelURL storageURL:(NSURL *)storageURL {
+    [self closeOpenFileSheetController];
+    
+    NSLog(@"Model URL: %@", modelURL);
+    NSLog(@"Storage URL: %@", storageURL);
 }
 
 @end
