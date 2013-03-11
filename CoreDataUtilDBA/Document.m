fafcb15f51467b08c777f940915d67168cc1059f
Aliaksandr Andrashuk <limejelly@yandex.ru>
2013-03-11 21:54:03 +0300

    Document based application added

---
+++ CoreDataUtilDBA/Document.m
@@ -0,0 +1,40 @@
+//
+//  Document.m
+//  CoreDataUtilDBA
+//
+//  Created by Aliksandr Andrashuk on 11.03.13.
+//  Copyright (c) 2013 mFluent LLC. All rights reserved.
+//
+
+#import "Document.h"
+
+@implementation Document
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"Document";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
++ (BOOL)autosavesInPlace
+{
+    return YES;
+}
+
+@end
---
+++ CoreDataUtilDBA/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreDataUtilDBA
+//
+//  Created by Aliksandr Andrashuk on 11.03.13.
+//  Copyright (c) 2013 mFluent LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
