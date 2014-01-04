29a4d09840126bff40dab250719b48da4c15075f
Chris Wilson <chris@mfluent.com>
2014-01-04 09:06:39 -0600

    Guards against a case (NSRelationshipDescription) that prevents predicate editor from showing.

--- CoreDataUtil/CoreData/MFLCoreDataIntrospection.m
+++ CoreDataUtil/CoreData/MFLCoreDataIntrospection.m
@@ -372,8 +372,12 @@
             // Not sure how to handle NSBinaryDataAttributeType so exlude from key paths
             //NSLog(@"Exclude NSBinaryDataAttributeType: %@", property);
             
-        } else {
+        } else if ([property isKindOfClass:NSAttributeDescription.class]) {
             [keyPaths addObject:[property name]];
+        } else {
+            // Need to handle NSRelationshipDescription
+            
+            NSLog(@"Unhandled NSAttributeDescription (%@)", [property entity]);
         }
         
     }
